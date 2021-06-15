75bb585b22b1a85dc22841304e379b2d3b3d6237
Ilhom Utkirov <46997626+Ilhom-Utkirov@users.noreply.github.com>
2021-06-15 13:59:46 +0500

    variables

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,20 +2,30 @@
  * @author : Ilhom O'tkirov
  * @created : 15.06.2021, вторник
  **/
- var greeting : String? = " nate"   // now nullable string
+ var greeting : String? = null   // now nullable string
  val  name : String = "hello kotlin"    //can be assigned only once
 
 fun main() {
-    println("Hello Kotlin")
-
+    println("start project!!! ")
     val name : String = "namelist"
 
-    println(name)
-    println(greeting)
 
-    greeting = null
+    when (greeting){  //switch
+        null-> println("Hi when it is null")  //case1
+        "comon"-> println("when it is comon") //case2
+        else-> println(greeting)               //default
+    }
 
-    println(name)
-    println(greeting)
+    val greetingToPrint = if(greeting != null) greeting else "Hi"
+
+    val greetingToPrint2 =when(greeting){
+        null->"Hi"
+        else->greeting
+    }
+
+    println("name = $name")
+    println("greetingToPrint =$greetingToPrint")
+    println("greetingToPrint =$greetingToPrint2")
+
 
 }

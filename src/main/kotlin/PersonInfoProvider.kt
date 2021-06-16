eab1f6a3b733a51e1ffbbd11aa932dccd739ef20
Ilhom Utkirov <46997626+Ilhom-Utkirov@users.noreply.github.com>
2021-06-16 12:47:50 +0500

    added interfaces

--- src/main/kotlin/PersonInfoProvider.kt
+++ src/main/kotlin/PersonInfoProvider.kt
@@ -2,6 +2,42 @@
  * @author : Ilhom O'tkirov
  * @created : 15.06.2021, вторник
  **/
-interface PersonInfoProvider
+interface PersonInfoProvider{
+    val providerInfo: String
 
-class BasicInfoProvider : PersonInfoProvider
+    fun printInfo(person:Person){
+        println( "basic info provider" )
+        person.printInfo()
+    }
+}
+
+interface SessionInfoProvider {
+    fun getSessionId() : String
+}
+
+
+//1 st class
+class BasicInfoProvider : PersonInfoProvider, SessionInfoProvider {
+
+    override val providerInfo: String
+        get() = "BasicInfoProvider"
+
+    override fun printInfo(person: Person) {
+        super.printInfo(person)
+        println("some additional statement")
+    }
+
+    override fun getSessionId(): String {
+        return "Session"
+    }
+
+}
+
+//main class
+fun main (){
+    val provider = BasicInfoProvider()
+
+    provider.printInfo(Person())
+    println("get Session id")
+    provider.getSessionId()
+}
